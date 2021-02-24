8dd8d5c19e8acd81ede4ef9b98d0f67c98c4563b
Martin <Martin@DESKTOP-82AB2HL>
2021-02-24 11:58:36 +0100

    Ajoutez des fichiers projet.

---
+++ QryptIdApp/QryptIdApp/Repositories/UserRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using QryptIdApp.Models;
+using System.Threading.Tasks;
+using QRCoder;
+
+namespace QryptIdApp.Repositories
+{
+    public class UserRepository
+    {
+        private SQLiteConnection connection;
+        public string StatusMessage { get; set; }
+
+        public UserRepository(string dbPath)
+        {
+            connection = new SQLiteConnection(dbPath);
+            connection.CreateTable<User>();
+            //ajout d'un user : Mbappé
+            connection.Insert(new User
+            {
+                Id = 1,
+                Nom = "Mbappé",
+                Prenoms = "Kylian",
+                Sexe = "M",
+                Birthday = "20/12/1998 à Paris",
+                Taille = 178,
+                Expiration = "10/03/2025",
+                Adresse = "12 rue Victor Hugo, 92200 Neuilly-sur-Seine",
+                Photo = "mbappe.bmp",
+                Signature = "mbappeSignature.bmp",
+
+                Email = "mbappe@gmail.com",
+                Password = "mbappe",
+                CodePin = ""
+
+            });
+            ;
+            //ajout d'un 2e user : 
+            connection.Insert(new User
+            {
+                Id = 2,
+                Nom = "Macron",
+                Prenoms = "Emmanuel",
+                Sexe = "M",
+                Birthday = "21/12/1977 à Amiens",
+                Taille = 173,
+                Expiration = "24/10/2028",
+                Adresse = "55 Rue du Faubourg Saint-Honoré, 75008 Paris",
+                Photo = "macron.bmp",
+                Signature = "macronSignature.bmp",
+
+                Email = "macron@gmail.com",
+                Password = "macron",
+                CodePin = ""
+
+            }); ;
+            connection.Insert(new User
+            {
+                Id = 3,
+                Nom = "Mouly",
+                Prenoms = "Vincent",
+                Sexe = "M",
+                Birthday = "27/05/2004 à Neuilly-sur-Seine",
+                Taille = 187,
+                Expiration = "05/06/2025",
+                Adresse = "52 rue Rivay, 92300 Levallois-Perret",
+                Photo = "vincent.bmp",
+                Signature = "vincentSignature.bmp",
+
+                Email = "vincent@gmail.com",
+                Password = "vincent",
+                CodePin = ""
+
+            });
+        }
+
+
+
+        public List<User> GetUsers()
+        {
+            try
+            {
+                return connection.Table<User>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Imposssible de recuperer la liste des CNI.\n Erreur : {ex.Message}";
+            }
+            return new List<User>();
+        }
+
+        public User LoginValidate(string userName1, string pwd1)
+        {
+            var data = connection.Table<User>();
+            var d1 = data.Where(x => x.Email == userName1 && x.Password == pwd1).FirstOrDefault();
+            
+            if (d1 != null)
+            {
+                return d1;
+            }
+            else
+                return null;
+        }
+
+        
+    }
+}
